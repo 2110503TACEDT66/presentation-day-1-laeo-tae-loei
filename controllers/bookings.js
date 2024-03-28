74484cb8fd5b33e922a01b9f9fe98629a15f1f53
martnarabodee <martnarabodee@gmail.com>
2024-03-28 22:39:40 +0700

    fix user unauthorize

--- controllers/bookings.js
+++ controllers/bookings.js
@@ -58,7 +58,7 @@ exports.getBooking= async (req,res,next)=>{
         }
         
         //Make sure user is the booking owner
-        if(booking.user.toString()!== req.user.id && req.user.role !== 'admin'){
+        if(booking.user.toString()!== req.user._id && req.user.role !== 'admin'){
 		    return res.status(401).json({success:false,message:`User ${req.user.id} is not authorized to update this booking`});
 	    }
 
